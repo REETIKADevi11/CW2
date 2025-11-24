95fb5b0a2765df17bcb849c25067b0b89c22bf0e
Reetika <reetikamandary11@gmail>
2025-11-24 22:22:22 +0400

    This code is about running the navigation

---
+++ home.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+
+
+home_page = st.Page(page = "stream/homePage.py",
+                    title = "Home Page")
+
+register_page = st.Page(page = "stream/registerPage.py",
+                     title = "Register"
+                     )
+
+login_page = st.Page(page = "stream/LoginPage.py",
+                     title = "Login")
+
+dashboard_page = st.Page(page = "stream/dashboard.py",
+                         title = "Dashboard" )
+
+
+
+pg = st.navigation({"Info":[home_page, register_page, login_page],"Domain": [dashboard_page]})
+
+pg.run()
