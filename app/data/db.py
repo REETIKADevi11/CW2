9a32e18d4c729b28bd49df5f879a239dc23364e2
Reetika <reetikamandary11@gmail>
2025-11-30 20:26:49 +0400

    connection of database

--- app/data/db.py
+++ app/data/db.py
@@ -1,8 +1,8 @@
 import sqlite3
 from pathlib import Path
-DB_PATH = Path("DATA") / "intelligence_platform.db"
+DB_PATH = Path("DATA") / "intelligences_platform.db"
 def connect_database(db_path = DB_PATH):
-    conn = sqlite3.connect('intelligence_platform.db')
+    conn = sqlite3.connect('DATA/intelligences_platform.db')
     return sqlite3.connect(str(db_path))
 
 
