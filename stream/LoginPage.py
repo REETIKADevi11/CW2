64a1a1c656db08081a6510097315903c0d993df6
Reetika <reetikamandary11@gmail>
2025-12-06 15:43:21 +0400

    Login page for user to access the domain

--- stream/LoginPage.py
+++ stream/LoginPage.py
@@ -3,7 +3,7 @@ import streamlit as st
 from PIL import Image
 from stream.dashboard import Dash, metaDash, ticketDash
 
-
+# This is the Login pase
 if "login" not in st.session_state:
     st.session_state.login = False 
 
@@ -11,7 +11,9 @@ if not st.session_state.login:
     with st.form("Login form"):
         st.header("Login form")
         img = Image.open("login.png")
-        st.image(img, width = 100)
+        col1 , col2, col3 = st.columns([1, 2, 1])
+        with col2:
+            st.image(img, width = 200)
         username = st.text_input("Enter username: ")
         password = st.text_input("Enter password: ", type = "password")
         submit_button = st.form_submit_button("Submit", type="primary")
@@ -26,13 +28,16 @@ if not st.session_state.login:
             else: 
                 st.success("you have succesfully login")
                 st.session_state.login = True
+ # the selection box will allow user to access which ever domain they want after login               
 if st.session_state.login == True:
    option = ["Cyber Dashboard", "IT ticket", "Dataset dashboard"]
    st.header ("Select dashboard: ")
-   dash_select = st.selectbox("Which dashboard would you like to access", options = option)
+   dash_select = st.selectbox("## Which dashboard would you like to access ##", options = option)
    if dash_select == "Cyber Dashboard":
        Dash()
+    
    elif dash_select == "IT ticket":
        ticketDash()
    elif dash_select == "Dataset dashboard":
        metaDash()
+
