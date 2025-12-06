ed140cf897504436eff16f62714e85184d67cbd8
Reetika <reetikamandary11@gmail>
2025-12-06 15:39:03 +0400

    CRUD operation for ticket

--- app/data/tickets.py
+++ app/data/tickets.py
@@ -1,45 +1,52 @@
 from app.data.db import connect_database
 import pandas as pd
+from app.data.schema import create_it_tickets_table, load_csv_to_table
 
+def insert_tickets( priority, description, status, assigned_to, created_date ,resolution_time_hours ):
+    create_it_tickets_table()
 
-def insert_tickets(conn, ticket_id, priority, status, assigned_to, created_at, resolution_time_hours):
     """Insert new datasets."""
-    conn = connect_database()
+    conn = connect_database("DATA/intelligences_platform.db")
     cursor = conn.cursor()
     cursor.execute("""
         INSERT INTO it_ticket
-        (ticket_id, priority, status, assigned_to, created_at, resolution_time_hours)
+        (priority,description, status, assigned_to,created_at,resolution_time_hours)
         VALUES (?, ?, ?, ?, ?, ?)
-    """, (ticket_id, priority, status, assigned_to, created_at, resolution_time_hours))
+    """, ( priority,description, status, assigned_to, created_date, resolution_time_hours))
     conn.commit()
     ticket_id = cursor.lastrowid
     conn.close()
     return ticket_id
 
 def get_all_ticket():
+    create_it_tickets_table()
+   
 #from CRUD operation here is the read part 
-    conn = connect_database()
+    conn = connect_database("DATA/intelligences_platform.db")
+    
     df = pd.read_sql_query(
-        "SELECT * it_ticket ORDER BY id DESC",
+        "SELECT * FROM it_ticket ORDER BY ticket_id DESC",
         conn
     )
+    print(df.to_string())
     conn.close()
     return df
 
 #from CRUD operation here is the update part 
-def update_ticket_status(conn, ticket_id, new_status):
+def update_ticket_status(ticket_id, new_status):
+    create_it_tickets_table()
     conn = connect_database()
     cursor = conn.cursor()
-    cursor.execute(""" UPDATE it_ticket SET status = ? WHERE ticket_id = ?""", (new_status, ticket_id))
+    cursor.execute("""UPDATE it_ticket SET status = ? WHERE ticket_id = ?""", (new_status, ticket_id))
     conn.commit()
     conn.close()
     return cursor.rowcount
 
 #from CRUD operation here is the delete part 
-def delete_ticket(conn, ticket_id):
-    conn = connect_database()
+def delete_ticket(ticket_id):
+    conn = connect_database("DATA/intelligences_platform.db")
     cursor = conn.cursor()
-    cursor.execute(""" DELETE FROM it_ticket WHERE ticket_id = ?"""(ticket_id))
+    cursor.execute("DELETE FROM it_ticket WHERE ticket_id = ?", (int(ticket_id),))
     conn.commit()
     conn.close()
     return cursor.rowcount
