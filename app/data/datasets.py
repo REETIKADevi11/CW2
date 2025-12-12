bd5de13d4c7e4467d5f8d0203a587b30eb77aed8
Reetika <reetikamandary11@gmail>
2025-12-12 09:39:58 +0400

    performing CRUD operation for data science domain

--- app/data/datasets.py
+++ app/data/datasets.py
@@ -1,28 +1,34 @@
 from app.data.db import connect_database
 import pandas as pd
+from app.data.schema import create_datasets_metadata_table, load_csv_to_table
 
 
-def insert_datasets(conn, dataset_id, name, rows, columns, uploaded_by, upload_date):
+def insert_datasets( name, rows, columns, uploaded_by, upload_date):
+    create_datasets_metadata_table()
     """Insert new datasets."""
-    conn = connect_database()
+    conn = connect_database("DATA/intelligences_platform.db")
     cursor = conn.cursor()
     cursor.execute("""
-        INSERT INTO datasets_metadata
-        (dataset_id, category, name, , rows, columns, uploaded_by, upload_date)
-        VALUES (?, ?, ?, ?, ?, ?)
-    """, (dataset_id, name, rows,columns, uploaded_by, upload_date))
+        INSERT INTO dataset_metadata
+        (name,  "rows", "columns", uploaded_by, upload_date)
+        VALUES ( ?, ?, ?, ?, ?)
+    """, (name, rows,columns, uploaded_by, upload_date))
     conn.commit()
-    dataset_id = cursor.lastrowid
+    insert= cursor.lastrowid
     conn.close()
-    return dataset_id
+    return insert
 
 def get_all_dataset():
+    create_datasets_metadata_table()
 #from CRUD operation here is the read part 
-    conn = connect_database()
+    conn = connect_database("DATA/intelligences_platform.db")
+
     df = pd.read_sql_query(
-        "SELECT * datasets_metadata ORDER BY dataset_id DESC",
+        "SELECT * FROM dataset_metadata ORDER BY dataset_id DESC",
         conn
     )
+    
+    print(df.to_string())
     conn.close()
     return df
 
@@ -30,16 +36,18 @@ def get_all_dataset():
 def update_dataset_status(conn, dataset_id, new_status):
     conn = connect_database()
     cursor = conn.cursor()
-    cursor.execute(""" UPDATE datasets_metadata SET status = ? WHERE dataset_id = ?""", (new_status, dataset_id))
+    cursor.execute(""" UPDATE dataset_metadata SET status = ? WHERE dataset_id = ?""", (new_status, dataset_id))
     conn.commit()
+    update = cursor.rowcount
     conn.close()
-    return cursor.rowcount
+    return update
 
 #from CRUD operation here is the delete part 
-def delete_dataset(conn, dataset_id):
-    conn = connect_database()
+def delete_dataset( dataset_id):
+    conn = connect_database("DATA/intelligences_platform.db")
     cursor = conn.cursor()
-    cursor.execute(""" DELETE FROM datasets_metadata WHERE dataset_id = ?"""(dataset_id))
+    cursor.execute(" DELETE FROM dataset_metadata WHERE dataset_id = ?", (int(dataset_id),))
     conn.commit()
+    delete = cursor.rowcount
     conn.close()
-    return cursor.rowcount
+    return delete
